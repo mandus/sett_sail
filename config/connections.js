ce88a567b5096363f16c92a51bcd626d530af2f0
Åsmund Ødegård <mandus@gmail.com>
2015-11-06 13:50:25 +0100

    Added MongoLabs db-adapter

--- config/connections.js
+++ config/connections.js
@@ -56,14 +56,19 @@ module.exports.connections = {
   * Run: npm install sails-mongo                                             *
   *                                                                          *
   ***************************************************************************/
-  localMongoDb: {
-    adapter: 'sails-mongo',
-    host: 'localhost',
-    port: 27017,
-    // user: 'username',
-    // password: 'password',
-    // database: 'your_mongo_db_name_here'
-  },
+//  localMongoDb: {
+//    adapter: 'sails-mongo',
+//    host: 'localhost',
+//    port: 27017,
+//    // user: 'username',
+//    // password: 'password',
+//    // database: 'your_mongo_db_name_here'
+//  },
+
+    MongoLabDb: {
+        adapter: 'sails-mongo',
+        url: process.env.MONGOLAB_URI
+    },
 
   /***************************************************************************
   *                                                                          *
